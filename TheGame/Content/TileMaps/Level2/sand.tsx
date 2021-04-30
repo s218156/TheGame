5b03b9661cf6fd035f76efc24b35346c08a4f14c
s218156 <201790@edu.p.lodz.pl>
2021-04-30 12:31:29 +0200

    expanded level 2

---
+++ TheGame/Content/TileMaps/Level2/sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="sand" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="../textures/mapTextures/sand.png" width="256" height="256"/>
+</tileset>
